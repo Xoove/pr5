86dbc65bac11f8da63b84e70ec02a4e635cb35c4
Xoove <You228ru@gmail.com>
2024-05-18 17:55:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pr5/Fragment3.java
@@ -0,0 +1,25 @@
+package com.example.pr5;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Fragment3 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment3_layout, container, false);
+
+        String receivedData = getArguments().getString("example");
+
+        TextView textView = view.findViewById(R.id.data_received_textview);
+        textView.setText(receivedData);
+
+        return view;
+    }
+}
+
